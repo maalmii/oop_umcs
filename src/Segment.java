90deedbc4d9e4a1de94095d6b151d54605184273
maalmii <mariamiro.1234.mm@gmail.com>
2024-03-13 17:09:11 +0100

    second2

---
+++ src/Segment.java
@@ -0,0 +1,12 @@
+public class Segment {
+    private final Point start;
+    private final  Point end;
+
+
+    public Segment(Point start, Point end) {
+        this.start = start;
+        this.end = end;
+    }
+
+
+}
